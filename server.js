0edbad2596f3111331fa511c1916942c4a298645
Joe Eames <joeeames@gmail.com>
2014-07-28 11:57:33 -0600

    finished with week 3

--- server.js
+++ server.js
@@ -1,14 +1,34 @@
-var express = require('express');
+var express = require('express'),
+    mongoose = require('mongoose');
 
 var app = express();
 
 app.set('views', __dirname + '/server/views');
 app.set('view engine', 'jade');
 
+mongoose.connect('mongodb://localhost/enbudget');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error...'));
+db.once('open', function() {
+  console.log('enbudget db opened');
+});
+
+var hwSchema = mongoose.Schema({
+  message: String
+});
+var hwModel = mongoose.model('message', hwSchema);
+
+var mongoMessage;
+hwModel.find({}).exec(function(err, collection) {
+  mongoMessage = collection[0].message;
+});
+
 app.use(express.static(__dirname + '/public'));
 
 app.get('*', function(req, res) {
-  res.render('index');
+  res.render('index', {
+    dbMessage: mongoMessage
+  });
 });
 
 var port = 8008;
